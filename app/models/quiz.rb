88e2ff15daa917b99594d4c62d530f464837b73d
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-27 02:49:33 +0000

    automatically backed up by learn

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,10 @@
 class Quiz
 
   @@questions = []
+  #an array holding the instances of the quiz.
+  @@results = []
+  #an array holding onto the possible results of the quiz.
+  def initialize(title)
+    @title = title
+  end 
 end
