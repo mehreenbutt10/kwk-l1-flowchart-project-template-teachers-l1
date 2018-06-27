07647252f61f2e3449068efe53999a85d5c65f0f
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-27 02:43:36 +0000

    automatically backed up by learn

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,11 @@
 class Question
-
+  def initialize(data{})
+    @id = data[:id]
+    @text = data[:text]
+    @answers = data[:answers]
 end
+
+question_1 = Question.new(:id => "Q1", :text =>"Are you tired?", :answers => {
+  "Yes" => "R2",
+  "No" => "Q2"
+  })
